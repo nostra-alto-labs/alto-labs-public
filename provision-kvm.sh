dfe6dc36f5afc23f4946988935346b5f3c5ea72e
Michal Garcarz <mgarcarz@cisco.com>
2022-09-15 11:10:50 +0200

    Adding fix for kernel upgrade

--- provision-kvm.sh
+++ provision-kvm.sh
@@ -8,6 +8,32 @@ rm -rf alto-labs
 
 echo "Welcome to the Alto NUC provisioning script."
 echo ""
+
+echo "Do you need to upgrade the kernel (required for the fist time you do provision Gen11) [Yes|No]:"
+read gen11
+if [ $gen11 == "Yes" ] 
+then
+  echo ""
+
+  # fix for https://gitlab.aicloud.cisco.com/raptor/labs/core/-/issues/83
+  echo "Gen11 selected, need to upgrade kernel to fix network driver bug"
+  sudo ip route del default || true
+  sudo dhclient br0 || true
+  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-headers-5.15.47-051547-generic_5.15.47-051547.202206141802_amd64.deb
+  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-headers-5.15.47-051547_5.15.47-051547.202206141802_all.deb
+  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-image-unsigned-5.15.47-051547-generic_5.15.47-051547.202206141802_amd64.deb
+  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-modules-5.15.47-051547-generic_5.15.47-051547.202206141802_amd64.deb
+
+  sudo dpkg -i *.deb || true
+  sudo apt -f install -y || true
+  sudo apt --fix-broken install -y || true
+  rm -rf linux-* || true
+  sync || true
+  echo "End of kernel patching for Gen11 - rebooting, once do please continue provisioning"
+  sleep 5
+  reboot -f
+fi
+
 ## Get SiteID:
 siteid="0"
 until [ $siteid -ge 201 ] && [ $siteid -le 399 ]
@@ -34,7 +60,6 @@ while ! python3 /opt/alto/bin/oauth.py; do
   echo ""
 done
 
-
 ### Change directory to alto-labs/ansible/ and setup pip environment.
 ### Note: we cannot use pipenv shell here.
 cd alto-labs/ansible/stratus/
@@ -51,36 +76,8 @@ while ! pipenv run ansible-playbook initial-provisioning.yml --extra-vars "targe
   echo "Retrying..."
 done
 
-### Run other tasks...
-echo "Is it Gen11 node [Yes|No]:"
-read gen11
-if [ $gen11 == "Yes" ] 
-then
-  echo ""
-  # to prevent c8k corruption, fix for https://gitlab.aicloud.cisco.com/raptor/labs/core/-/issues/107
-  echo "Preventing c8k corruption"
-  sudo virsh destroy c8k --graceful || true
-  sync || true
-
-  # fix for https://gitlab.aicloud.cisco.com/raptor/labs/core/-/issues/83
-  echo "Gen11 selected, need to upgrade kernel to fix network driver bug"
-  sudo ip route del default || true
-  sudo dhclient br0 || true
-  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-headers-5.15.47-051547-generic_5.15.47-051547.202206141802_amd64.deb
-  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-headers-5.15.47-051547_5.15.47-051547.202206141802_all.deb
-  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-image-unsigned-5.15.47-051547-generic_5.15.47-051547.202206141802_amd64.deb
-  wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.15.47/amd64/linux-modules-5.15.47-051547-generic_5.15.47-051547.202206141802_amd64.deb
-
-  sudo dpkg -i *.deb || true
-  sudo apt -f install -y || true
-  sudo apt --fix-broken install -y || true
-  rm -rf linux-* || true
-  sync || true
-  echo "End of kernel patching for Gen11 - please reboot after initial provisioning is finished"
-fi
-
 echo ""
-echo "End of initial provisioning"
+echo "End of initial provisioning."
 sleep 5
 ### Remove alto-labs directory
 rm -rf ~/alto-labs
